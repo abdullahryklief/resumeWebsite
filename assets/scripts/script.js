74a18c7332afef9243092ee0e58ba342b939d53e
AbdullahCodespace <abdullah@codespace.co.za>
2022-05-31 21:11:33 +0200

    updated nav and added some content

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,22 @@
-function openNav() {
-    document.getElementById("sideNav").style.width = "15%";
-}
+// function openNav() {
+//     document.getElementById("sideNav").style.width = "15%";
+//     document.getElementById("sideNav").style.height = "100%"; 
+// }
 
-function closeNav() {
-    document.getElementById("sideNav").style.width = "0";
+// function closeNav() {
+//     document.getElementById("sideNav").style.width = "0";
+//     document.getElementById("sideNav").style.height = "0";
+// }
+
+function toggleNav() {
+    if(document.getElementById("sideNav").style.width == "15%"){
+        document.getElementById("sideNav").style.width = "0";
+        document.getElementById("sideNav").style.height = "0";
+        document.getElementById("menuBtn").innerHTML = "&#9776;";
+        document.getElementById("menuBtn").style.color = "black";
+    }else{
+        document.getElementById("sideNav").style.width = "15%";
+        document.getElementById("sideNav").style.height = "100%"; 
+        document.getElementById("menuBtn").style.color = "white";
+    }
 }
